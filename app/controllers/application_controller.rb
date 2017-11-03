5b05c5991aaf282c18dddef53f436115088c67aa
Bryancm <bryan.mtzs@gmail.com>
2017-11-02 20:36:48 -0500

    permitir parametros ademas de correo y password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     update_attrs = [:password, :password_confirmation, :current_password]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:lastName,:phone,:street,:exterior_number,:interior_number,:city,:state,:zip,])
   end
 end
